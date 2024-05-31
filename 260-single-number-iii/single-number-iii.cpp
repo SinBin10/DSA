aec122c60a5bda8ebd0c1d0dfc8af75e0260704d
Binay Pratap Singh <118094656+SinBin10@users.noreply.github.com>
2024-06-01 01:23:23 +0530

    Time: 6 ms (68.76%) | Memory: 14.3 MB (9.91%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int n1 = 0,n2 = 0;
+        int check = true;
+        for(int i = 0;i<nums.size();i++)
+        ++mp[nums[i]];
+        for(auto it:mp){
+            if(it.second == 1){
+                if(check){
+                    n1 = it.first;
+                    check = false;
+                }
+                else
+                n2 = it.first;
+            }
+        }
+        return {n1,n2};
+    }
+};
