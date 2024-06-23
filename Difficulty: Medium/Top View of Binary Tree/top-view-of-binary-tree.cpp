a0f269308452cf953d0175ecbebf879c14e3ea33
Binay Pratap Singh <118094656+SinBin10@users.noreply.github.com>
2024-06-23 10:56:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        map<int,int> mp;
+        stack<pair<int,Node*>> st1;
+        stack<pair<int,Node*>> st2;
+        st1.push({0,root});
+        mp[0] = root->data;
+        while(!st1.empty()){
+            while(!st1.empty()){
+                Node* node = st1.top().second;
+                int col = st1.top().first;
+                if(node->left!=NULL){
+                    st2.push({col-1,node->left});
+                    if(mp.find(col-1) == mp.end())
+                    mp[col-1] = node->left->data;
+                }
+                if(node->right!=NULL){
+                    st2.push({col+1,node->right});
+                    if(mp.find(col+1) == mp.end())
+                    mp[col+1] = node->right->data;
+                }
+                st1.pop();
+            }
+            while (!st2.empty()) {
+                Node* node = st2.top().second;
+                if(node!=NULL)
+                st1.push(st2.top());
+                st2.pop();
+            }
+        }
+        // for(auto it:mp)
+        // cout<<it.first<<" "<<it.second<<endl;
+        for(auto it:mp)
+        ans.push_back(it.second);
+        return ans;
+    }
+
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin>>tc;
+    cin.ignore(256, '\n');
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        vector<int> vec = ob.topView(root);
+        for(int x : vec)
+            cout<<x<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
