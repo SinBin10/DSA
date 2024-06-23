99b06d82c4daab8482222508d0c6135ead30c6df
Binay Pratap Singh <118094656+SinBin10@users.noreply.github.com>
2024-06-23 11:50:50 +0530

    Time: 4 ms (32.52%) | Memory: 14.7 MB (10.47%) - LeetSync

--- Leetcode/199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ Leetcode/199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,32 +14,33 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         if(root == NULL)
         return {};
+        int row = 1;
         vector<int> ans;
         map<int,int> mp;
-        stack<pair<int,TreeNode*>> st1;
-        stack<pair<int,TreeNode*>> st2;
-        st1.push({0,root});
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        st1.push(root);
         mp[0] = root->val;
         while(!st1.empty()){
             while(!st1.empty()){
-                TreeNode* node = st1.top().second;
-                int col = st1.top().first;
+                TreeNode* node = st1.top();
                 if(node->left!=NULL){
-                    st2.push({col+1,node->left});
-                    mp[col+1] = node->left->val;
+                    st2.push(node->left);
+                    mp[row] = node->left->val;
                 }
                 if(node->right!=NULL){
-                    st2.push({col+1,node->right});
-                    mp[col+1] = node->right->val;
+                    st2.push(node->right);
+                    mp[row] = node->right->val;
                 }
                 st1.pop();
             }
             while (!st2.empty()) {
-                TreeNode* node = st2.top().second;
+                TreeNode* node = st2.top();
                 if(node!=NULL)
                 st1.push(st2.top());
                 st2.pop();
             }
+            ++row;
         }
         for(auto it:mp)
         ans.push_back(it.second);
